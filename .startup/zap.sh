bed45c4a1ad27cfb309139f1b988b8f262635265
Daniel Tolan <danielt812@gmail.com>
2023-05-26 09:29:31 -1000

    deleting .startup dir

--- .startup/zap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-# https://github.com/zap-zsh/zap
-if [[ -d $ZAP_DIR ]]; then
-	echo "zap-zsh is already installed."
-else
-	echo "installing zap-zsh..."
-	zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
-fi
