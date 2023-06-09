ad8061d8340a2952b7d37368b82e265adea12ba6
Daniel Tolan <danieltolan@gmail.com>
2023-06-09 13:53:56 -1000

    adding functions for parse git branch

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,14 +1,9 @@
-# Git branch in prompt.
-function parse_git_branch() {
-	git branch 2> /dev/null | sed -n -e 's/^\* \(.*\)/(\1)/p'
-}
-
 # Automate git merges/pushes to master and staging branch
 function alameda_push() {
-  local branch=$(git branch 2> /dev/null | sed -n -e 's/^\* \(.*\)/\1/p')
+  local branch=$(git branch --show-current 2> /dev/null)
   local flag=$1
 
-  if [[ $branch == 'master' || $branch == 'staging' ]];then
+  if [[ $branch =~ ^(master|staging)$ ]]; then
     echo 'Already on master or staging branch'
   else
     if [[ $flag == '-a' ]]; then
@@ -19,9 +14,9 @@ function alameda_push() {
       gpush && gcos && gm $branch && gpush && gco $branch
     elif [[ $flag == '-h' ]]; then
       echo 'Usage:'
-      echo '-a: Merge/Push to master and staging'
-      echo '-m: Merge/Push to master only'
-      echo '-s: Merge/Push to staging only'
+      echo '-a: Push/Merge to master and staging'
+      echo '-m: Push/Merge to master only'
+      echo '-s: Push/Merge to staging only'
       echo '-h: Show help information'
     else
       echo 'Invalid flag provided'
@@ -30,11 +25,18 @@ function alameda_push() {
 }
 
 function alameda_path() {
-  cd $HOME/mcisemi/alameda/www.mcisemi.com/$1
-  gco $1 2> /dev/null
-  gpull
+  cd $HOME/mcisemi/alameda/www.mcisemi.com
+  git checkout master
+  git pull
+  cd $1
+  git checkout $1 2> /dev/null
+  git pull
 
   local url=$(basename $(pwd))
   code .
   open -a $BROWSER "http://mcisemi.alameda.test/$url"
+}
+
+batdiff() {
+  git diff --name-only --relative --diff-filter=d | xargs bat --diff
 }
