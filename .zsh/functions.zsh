bd7729bb3cbf5f85b7dc3eb19dd9102f17f3fb2b
Daniel Tolan <danieltolan@gmail.com>
2023-06-28 21:07:40 -1000

    unbind kitty new tab, write helper function for keybinds

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,3 +1,49 @@
+# Print Config Func
+pc() {
+  local arg=$1
+  local flag=$2
+  if [[ $arg == 'help' ]]; then
+    echo 'Usage:'
+    echo 'tmux [options]: show tmux keybinds/options'
+    echo 'lvim [options]: show lvim keybinds/options'
+    echo 'help: show help information'
+  elif [[ $arg == 'tmux' ]]; then
+    if [[ $flag == '' ]]; then
+      echo 'Use flag option'
+      echo '-p "pain_control.tmux"'
+      echo '-s "sensible.tmux"'
+      echo '-r "resurrect.tmux"'
+      echo '-m "continuum.tmux"'
+      echo '-y "yank.tmux"'
+      echo '-c "copycat.tmux"'
+    elif [[ $flag == '-p' ]]; then
+      bat $CONFIG/tmux/plugins/tmux-pain-control/pain_control.tmux
+    elif [[ $flag == '-s' ]]; then
+      bat $CONFIG/tmux/plugins/tmux-sensible/sensible.tmux
+    elif [[ $flag == '-r' ]]; then
+      bat $CONFIG/tmux/plugins/tmux-resurrect/resurrect.tmux
+    elif [[ $flag == '-m' ]]; then
+      bat $CONFIG/tmux/plugins/tmux-continuum/continuum.tmux
+    elif [[ $flag == '-y' ]]; then
+      bat $CONFIG/tmux/plugins/tmux-yank/yank.tmux
+    elif [[ $flag == '-c' ]]; then
+      bat $CONFIG/tmux/plugins/tmux-copycat/copycat.tmux
+    fi
+  elif [[ $arg == 'lvim' ]]; then
+    if [[ $flag == '' ]]; then
+      echo 'Use flag option'
+      echo '-k "keymappings.lua"'
+      echo '-s "settings.lua"'
+    elif [[ $flag == '-k' ]]; then
+      bat $HOME/.local/share/lunarvim/lvim/lua/lvim/keymappings.lua
+    elif [[ $flag == '-s' ]]; then
+      bat $HOME/.local/share/lunarvim/lvim/lua/lvim/config/settings.lua
+    fi
+  else
+    echo 'Command not found try help'
+  fi
+}
+
 # Automate git merges/pushes to master and staging branch
 function alameda_push() {
   local branch=$(git branch --show-current 2> /dev/null)
