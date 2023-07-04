26202cdfa02d7ad4f894ca2ede7a0d1b401dedae
Daniel Tolan <danieltolan@gmail.com>
2023-07-03 18:26:10 -1000

    set gitblame to 1 by default

--- .lvim/config.lua
+++ .lvim/config.lua
@@ -25,7 +25,7 @@ lvim.plugins = {
     event = "BufRead",
     config = function()
       vim.cmd "highlight default link gitblame SpecialComment"
-      vim.g.gitblame_enabled = 0
+      vim.g.gitblame_enabled = 1
     end,
   },
   {
