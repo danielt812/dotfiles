a039d4dddda14c07724cc21035de4b3fc162e1e8
Daniel Tolan <danieltolan@gmail.com>
2023-08-10 23:39:05 -1000

    ZSH Functions added
    alias fzf function added
    kill port function added

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -56,7 +56,7 @@ summary() {
   echo 'Full Stack Web Developer with 5+ years of expertise known for driving solutions, proven success, and maximizing your organizational growth. I consistently stay up to date with the latest industry trends and technologies to create engaging and user-friendly experiences. Proficient in the MERN Stack (MongoDB | Express | React | Node). I also possess advanced knowledge of JavaScript and have hands-on virtuosity with various libraries and frameworks. In my spare time, I like surfing, playing guitar, spending time with my cat, and expanding knowledge to be a command line power user' | cat
 }
 
-# Shortcuts to read markdown files
+# Markdown
 md() {
   local arg=$1
   local flag=$2
@@ -105,11 +105,29 @@ md() {
   fi
 }
 
-pyutils() {
+# Python Utils
+pyu() {
   local arg=$1
   if [[ $arg == 'str' ]]; then
-    py $HOME/PythonUtils/strCase.py $@
+    py $HOME/PythonUtils/str.py $@
   else
     echo "Unknown command"
+  fi
+}
+
+# Kill Port
+kp() {
+  local port=$1
+  echo "Killing port ${port}..."
+  lsof -i :"${port}" | awk '{print $2}' | grep -v PID | xargs kill
+}
+
+# Alias FZF
+af() {
+  local selected_alias
+  selected_alias=$(alias | fzf | cut -d '=' -f 1)
+  if [[ -n $selected_alias ]]; then
+    eval "$selected_alias"
+    print -s !$
   fi
 }
