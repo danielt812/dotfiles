7578ab20edeb6a603681e4efebee8b9be96fc943
Daniel Tolan <danieltolan@gmail.com>
2024-01-05 08:39:23 -1000

    add alameda_push function back in

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -74,6 +74,32 @@ af() {
   fi
 }
 
+# Automate git merges/pushes to master and staging branch
+alameda_push() {
+  local branch=$(git branch --show-current 2> /dev/null)
+  local flag=$1
+
+  if [[ $branch =~ ^(master|staging)$ ]]; then
+    echo 'Already on master or staging branch'
+  else
+    if [[ $flag == '-a' ]]; then
+      gpull && gpush && gcom && gm $branch --no-edit && gpush && gcos && gm $branch --no-edit && gpush && gco $branch
+    elif [[ $flag == '-m' ]]; then
+      gpull && gpush && gcom && gm $branch --no-edit && gpush && gco $branch
+    elif [[ $flag == '-s' ]]; then
+      gpull && gpush && gcos && gm $branch --no-edit && gpush && gco $branch
+    elif [[ $flag == '-h' ]]; then
+      echo 'Usage:'
+      echo '-a: Push/Merge to master and staging'
+      echo '-m: Push/Merge to master only'
+      echo '-s: Push/Merge to staging only'
+      echo '-h: Show help information'
+    else
+      echo 'Invalid flag provided'
+    fi
+  fi
+}
+
 alameda_path() {
   local url
   cd "$HOME/mcisemi/alameda/www.mcisemi.com" || exit
@@ -125,5 +151,6 @@ gbf() {
   else
     selected_branch=$(git branch | fzf | tr -d '[:blank:]')
     git checkout "$selected_branch"
+    git pull
   fi
 }
