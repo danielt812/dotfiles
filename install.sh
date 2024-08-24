14ee937528668f1bf1c363676874400d01608cb1
Daniel Tolan <danieltolan@gmail.com>
2024-08-24 00:59:46 -1000

    add lazygit to packages list

--- install.sh
+++ install.sh
@@ -186,7 +186,7 @@ install_font() {
 # Main script logic
 OS=$(detect_os)
 PACKAGE_MANAGER=$(get_package_manager)
-PACKAGES="bat bottom fd-find fzf git-delta kitty lsd neofetch neovim ripgrep sd starship stow tealdeer tmux zoxide zsh"
+PACKAGES="bat bottom fd-find fzf git-delta kitty lazygit lsd neofetch neovim ripgrep sd starship stow tealdeer tmux zoxide zsh"
 
 if [ "$OS" == "Linux" ]; then
 	echo "Operating System: Linux"
