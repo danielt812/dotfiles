4d7d29402426226c1cf1d670de9ac72aa1eac095
Daniel Tolan <danieltolan@gmail.com>
2024-10-30 19:38:13 -1000

    add protontricks aliases to run from flatpak

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -66,3 +66,6 @@ alias dotfiles="$EDITOR $DOTFILES"
 
 # Weather
 alias weather="curl wttr.in"
+
+alias protontricks="flatpak run com.github.Matoking.protontricks"
+alias protontricks-launch="flatpak run --command=protontricks-launch com.github.Matoking.protontricks"
